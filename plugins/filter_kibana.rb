a7ea8e1750a977de0607712ebd7e7d4e30cf55c1
Brian Dentino <brian.dentino@gmail.com>
2020-01-12 13:19:45 -0600

    Fix kibana log format

--- plugins/filter_kibana.rb
+++ plugins/filter_kibana.rb
@@ -1,30 +1,16 @@
-require 'fluent/filter'
+require 'fluent/plugin/filter'
 
-module Fluent
-  class ExpatFilter < Filter
-    Plugin.register_filter('kibana', self)
+module Fluent::Plugin
+  class KibanaFilter < Filter
+    Fluent::Plugin.register_filter('kibana', self)
 
     config_param :hostname, :string, default: '<unknown>'
-
-    def initialize
-      super
-    end
-
-    def configure(conf)
-      super
-    end
-
-    def start
-      super
-    end
-
-    def shutdown
-      super
-    end
 
     def filter(tag, time, record)
       record['@timestamp'] = Time.at(time.to_r).utc.strftime('%Y-%m-%dT%H:%M:%S.%LZ')
       record['host.name'] = @hostname
+      record['message'] = record['log']
+      record.delete('log')
       record
     end
   end
